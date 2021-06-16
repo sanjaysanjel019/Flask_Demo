ea15062f7f951496b9380cb1d2d0f5ac2b41866c
Sanjay Sanjel <sanjay.sanjel018@gmail.com>
2021-06-16 20:57:39 +0545

    Added registration and login

--- flask_blog.py
+++ flask_blog.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.debug = True
+
+app.config['SECRET_KEY'] = 'thisisjustasupersecretrandomkey123'
+
 posts = [
     {
         'author': 'Pop Minho',
@@ -24,6 +27,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title="About Page")
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title="Register", form=form)
+    
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have successfully logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password','danger')
+    return render_template('login.html',title="Login",form=form)
 
 if __name__ == "__main__":
     app.run(debug=True) 
